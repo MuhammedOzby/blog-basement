88efd21577e5a1407db42e33ca18dfad8869b751
Muhammed Mustafa ÖZBAY <muhammed.ozby@outlook.com>
2023-02-07 15:50:45 +0300

    Database setting out constant

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -5,20 +5,18 @@ import { BlogModule } from './blog/blog.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Post } from './Entities/Post.enitity';
 
+const DatabaseSettings = TypeOrmModule.forRoot({
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: 'devdb',
+  database: 'test',
+  entities: [Post],
+  synchronize: true,
+});
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: 'localhost',
-      port: 3306,
-      username: 'root',
-      password: 'devdb',
-      database: 'test',
-      entities: [Post],
-      synchronize: true,
-    }),
-    BlogModule,
-  ],
+  imports: [DatabaseSettings, BlogModule],
   controllers: [AppController],
   providers: [AppService],
 })
