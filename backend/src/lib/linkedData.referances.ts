0761e85e9b03129a36fa0a445c5bd3a3b276c13b
Muhammed Mustafa ÖZBAY <muhammed.ozby@outlook.com>
2023-02-20 14:41:21 +0300

    Create linkedData.referances.ts

---
+++ backend/src/lib/linkedData.referances.ts
@@ -0,0 +1,37 @@
+export interface Person {
+  '@context': 'https://schema.org/';
+  '@type': 'Person';
+  address?: Address;
+  email: string;
+  image?: string;
+  jobTitle: string;
+  name: string;
+  telephone: string;
+  url: string;
+}
+
+export interface Address {
+  '@context': 'https://schema.org/';
+  '@type': 'PostalAddress';
+  addressLocality: string;
+  addressRegion: string;
+  postalCode: string;
+  streetAddress: string;
+}
+
+export type Article = {
+  '@context': 'https://schema.org/';
+  '@type': 'Article';
+  headline: string;
+  alternativeHeadline: string;
+  genre: string;
+  keywords: string;
+  description: string;
+  articleBody: string;
+  datePublished: Date;
+  dateCreated: Date;
+  dateModified: Date;
+  author: Person;
+  editor: Person;
+  publisher: Person;
+};
