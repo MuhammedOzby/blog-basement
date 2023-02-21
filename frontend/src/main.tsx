f5fc34d4eb910a109ce8b06f5bbfccb09f884100
Muhammed Mustafa ÖZBAY <muhammed.ozby@outlook.com>
2023-02-22 00:58:48 +0300

    CSS ekleme mthjax için

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,5 +4,6 @@ import { RouterProvider } from 'react-router-dom';
 import { router } from './lib/Routes';
 import './assets/css/bulma.min.css';
 import './assets/css/styles.css';
+import 'katex/dist/katex.min.css';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(<RouterProvider router={router} />);
