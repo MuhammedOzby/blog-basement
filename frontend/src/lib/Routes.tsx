11ecbeee6dd006e9aa27f17bc45d0103cf6ed482
Muhammed Mustafa ÖZBAY <muhammed.ozby@outlook.com>
2023-02-22 00:59:04 +0300

    Article dosyaları için düzenleme

--- frontend/src/lib/Routes.tsx
+++ frontend/src/lib/Routes.tsx
@@ -1,9 +1,9 @@
 import { createBrowserRouter } from 'react-router-dom';
 import AboutPages from '../Pages/About.pages';
 import ContactPages from '../Pages/Contact.pages';
-import ExamplePage from '../Pages/Example.pages';
+import ArticlesPage from '../Pages/Articles.pages';
 import MainPages from '../Pages/Main.pages';
-import MakalePage from '../Pages/Makale.pages';
+import ArticlePage from '../Pages/Article.pages';
 import ResumePages from '../Pages/Resume.pages';
 import WelcomePages from '../Pages/Welcome.pages';
 
@@ -13,13 +13,13 @@ export const router = createBrowserRouter([
 		element: <MainPages />,
 		children: [
 			{ element: <WelcomePages />, index: true },
-			{ path: '/articles', element: <ExamplePage /> },
+			{ path: '/articles', element: <ArticlesPage /> },
 			{ path: '/resume', element: <ResumePages /> },
 			{ path: '/about-me', element: <AboutPages /> },
 			{ path: '/contact', element: <ContactPages /> },
 			{
-				path: '/article/:articleID',
-				element: <MakalePage />,
+				path: '/articles/:articleID',
+				element: <ArticlePage />,
 			},
 		],
 	},
