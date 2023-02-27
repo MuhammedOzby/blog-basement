4ad4ed61c19fa90d81622ac907494b9258a99e93
Muhammed Mustafa ÖZBAY <muhammed.ozby@outlook.com>
2023-02-27 21:12:21 +0300

    Update Welcome.pages.tsx

--- frontend/src/Pages/Welcome.pages.tsx
+++ frontend/src/Pages/Welcome.pages.tsx
@@ -1,19 +1,18 @@
-import React, { Component } from "react";
-import { ReactMarkdown } from "react-markdown/lib/react-markdown";
-import { getContent } from "../lib/getDataFromGit";
+import React, { Component } from 'react';
+import { ReactMarkdown } from 'react-markdown/lib/react-markdown';
 
 export default class WelcomePages extends Component<{}, { icerik: string }> {
-  constructor(props: { params: { dosyaYolu: string } }) {
-    super(props);
-    this.state = { icerik: "" };
-  }
-  async componentDidMount(): Promise<void> {
-    this.setState({
-      ...this.state,
-      icerik: await getContent("Karşılama Yazısı"),
-    });
-  }
-  render() {
-    return <ReactMarkdown children={this.state.icerik} />;
-  }
+	constructor(props: { params: { dosyaYolu: string } }) {
+		super(props);
+		this.state = { icerik: '' };
+	}
+	async componentDidMount(): Promise<void> {
+		this.setState({
+			...this.state,
+			icerik: 'karşılama yazısı',
+		});
+	}
+	render() {
+		return <ReactMarkdown children={this.state.icerik} />;
+	}
 }
